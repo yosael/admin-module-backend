03c8d90162c3fffd92a9f7b3e26b4fcef4a968b8
Edwin Gutierrez <yosael.gutierrez@gmail.com>
2023-04-25 09:26:09 -0600

    Project Setup

---
+++ src/main/java/com/app/adminmodulebackend/AdminModuleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.adminmodulebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminModuleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminModuleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/adminmodulebackend/AdminModuleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.adminmodulebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminModuleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
